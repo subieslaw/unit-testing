0e9cca3332f3132449cf7a739a1d0b75984c3fa7
Paweł Urbanik <subieslaw@gmail.com>
2021-07-21 22:48:40 +0200

    add rule description and reason description for existing test, new test for Interfaces

--- demo/src/test/java/org/subieslaw/finance/architecture/NamingConventionsTest.java
+++ demo/src/test/java/org/subieslaw/finance/architecture/NamingConventionsTest.java
@@ -12,9 +12,17 @@ import com.tngtech.archunit.core.importer.ImportOption;
 public class NamingConventionsTest {
     
     @ArchTest
-    static ArchRule controllers_should_not_have_Gui_in_name =
+    static ArchRule classes_should_not_have_demo_in_name =
             classes()
                     .that().resideInAPackage("..demo..")
-                    .should().haveSimpleNameNotContaining("Demo");
+                    .should().haveSimpleNameNotContaining("Demo")
+                    .as("Classes should have meaningful names")
+                    .because("Names should have business meaning and should not introduce another place for confusion.");
+
+    @ArchTest
+    static ArchRule interfaces_should_not_have_interface_in_name =
+            classes()
+                    .that().areInterfaces()
+                    .should().haveSimpleNameNotContaining("Interface");
 
 }
